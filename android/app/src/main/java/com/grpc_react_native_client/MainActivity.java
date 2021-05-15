9bf732564110debec76b6f1df054e3fe8595ce69
Gleb Sapegin <gleb@inperium.com>
2021-05-15 19:45:38 +0300

    Initial commit

---
+++ android/app/src/main/java/com/grpc_react_native_client/MainActivity.java
@@ -0,0 +1,15 @@
+package com.grpc_react_native_client;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "gRPC_React_Native_Client";
+  }
+}
